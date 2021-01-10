1a46cf7445041885b12c2139f0c454fa19a2c1ed
Biba-dev <hiba.awad@etu.emse.fr>
2021-01-10 13:16:01 +0100

    First commit

---
+++ app/src/main/java/com/example/faircorp/OnWindowSelectedListener.kt
@@ -0,0 +1,7 @@
+package com.example.faircorp
+
+interface OnWindowSelectedListener {
+
+
+    fun onWindowSelected(id: Long)
+}
---
+++ app/src/main/java/com/example/faircorp/model/WindowDto.kt
@@ -0,0 +1,3 @@
+package com.example.faircorp.model
+enum class Status { OPEN, CLOSED}
+data class WindowDto(val id: Long, val name: String, val room: RoomDto, val status: Status)
